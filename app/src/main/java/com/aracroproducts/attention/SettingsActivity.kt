9e895a98d39431c272be9f2eda498f4d98c689bc
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2021-12-06 08:45:54 -0800

    Continued to working on migration to compose

--- app/src/main/java/com/aracroproducts/attention/SettingsActivity.kt
+++ app/src/main/java/com/aracroproducts/attention/SettingsActivity.kt
@@ -11,12 +11,13 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
-        supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
-                .commit()
-        val actionBar = supportActionBar
-        actionBar?.setDisplayHomeAsUpEnabled(true)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.settings, SettingsFragment())
+                    .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     /**
