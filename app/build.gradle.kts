5c2a37b9dcf6ade8d6994ea15ed4e1f860103138
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2022-11-28 22:52:58 -0800

    Incremented version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.aracroproducts.attentionv2"
         minSdk = 24
         targetSdk = 33
-        versionCode = 32
-        versionName = "2.1.3"
+        versionCode = 33
+        versionName = "2.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
