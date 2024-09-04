18c0a77f148784b42cdd104785ad1c0439f874ca
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2024-09-04 10:38:15 -0700

    Added new database schema, optimized imports

--- app/src/main/kotlin/com/aracroproducts/attentionv2/NetworkUtils.kt
+++ app/src/main/kotlin/com/aracroproducts/attentionv2/NetworkUtils.kt
@@ -6,10 +6,19 @@ import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import okio.BufferedSink
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.*
+import retrofit2.http.DELETE
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Part
+import retrofit2.http.Path
+import retrofit2.http.Query
 import java.io.IOException
 import java.io.InputStream
 
